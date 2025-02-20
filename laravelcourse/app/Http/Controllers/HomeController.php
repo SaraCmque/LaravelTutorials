d0198b8d382ee54fab4d529823bc1f70a0152ae0
SaraCmque <saramanrique2016@gmail.com>
2025-02-19 22:18:46 -0500

    Tutorial 2 finished

--- laravelcourse/app/Http/Controllers/HomeController.php
+++ laravelcourse/app/Http/Controllers/HomeController.php
@@ -1,38 +1,40 @@
-<?php 
- 
+<?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request; 
- 
-class HomeController extends Controller 
-{ 
-  public function index(): View 
-  { 
-    $viewData = []; 
-    $viewData["title"] = "Home Page - Online Store"; 
-    return view('home.index')->with("viewData", $viewData); 
-  } 
- 
-  public function about(): View 
-  { 
-    $viewData = []; 
-    $viewData["title"] = "About us - Online Store"; 
-    $viewData["subtitle"] =  "About us"; 
-    $viewData["description"] =  "This is an about page ..."; 
-    $viewData["author"] = "Developed by: Sara Cortes"; 
-    return view('home.about')->with("viewData", $viewData); 
- 
-  } 
 
-  public function contact(): View
-  {
-    $viewData = [];
-    $viewData["title"] = "Contact - Online Store"; 
-    $viewData["subtitle"] =  "Contact"; 
-    $viewData["email"] = "info@gmail.com";
-    $viewData["address"] = "Cr 13 No. 90-36 OF 502";
-    $viewData["phoneNumber"] = "+57 123 456 789";
-    return view('home.contact')->with("viewData", $viewData);
-  }
-} 
+class HomeController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Home Page - Online Store';
+
+        return view('home.index')->with('viewData', $viewData);
+    }
+
+    public function about(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'About us - Online Store';
+        $viewData['subtitle'] = 'About us';
+        $viewData['description'] = 'This is an about page ...';
+        $viewData['author'] = 'Developed by: Sara Cortes';
+
+        return view('home.about')->with('viewData', $viewData);
+
+    }
+
+    public function contact(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Contact - Online Store';
+        $viewData['subtitle'] = 'Contact';
+        $viewData['email'] = 'info@gmail.com';
+        $viewData['address'] = 'Cr 13 No. 90-36 OF 502';
+        $viewData['phoneNumber'] = '+57 123 456 789';
+
+        return view('home.contact')->with('viewData', $viewData);
+    }
+}
--- laravelcourse/app/Http/Controllers/ProductController.php
+++ laravelcourse/app/Http/Controllers/ProductController.php
@@ -2,68 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id" => "1", "name" => "TV", "description" => "Best TV", "price" => 499.99],
-        ["id" => "2", "name" => "iPhone", "description" => "Best iPhone", "price" => 999.99],
-        ["id" => "3", "name" => "Chromecast", "description" => "Best Chromecast", "price" => 49.99],
-        ["id" => "4", "name" => "Glasses", "description" => "Best Glasses", "price" => 199.99]
-    ];
-
     public function index(): View
     {
         $viewData = [];
-        $viewData["title"] = "Products - Online Store";
-        $viewData["subtitle"] = "List of products";
-        $viewData["products"] = ProductController::$products;
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
 
-        return view('product.index')->with("viewData", $viewData);
+        return view('product.index')->with('viewData', $viewData);
     }
 
     public function show(string $id): View|RedirectResponse
     {
         $viewData = [];
 
-        // Validate if ID is valid
-        if (!isset(ProductController::$products[$id - 1])) {
-            return redirect()->route('home.index'); // Redirect to home page
-        }
+        // // Validate if ID is valid
+        // if (!isset(ProductController::$products[$id - 1])) {
+        //     return redirect()->route('home.index'); // Redirect to home page
+        // }
 
-        $product = ProductController::$products[$id - 1];
-        $viewData["title"] = $product["name"] . " - Online Store";
-        $viewData["subtitle"] = $product["name"] . " - Product information";
-        $viewData["product"] = $product;
+        $product = Product::findOrFail($id);
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
 
-        return view('product.show')->with("viewData", $viewData);
+        return view('product.show')->with('viewData', $viewData);
     }
 
     public function create(): View
     {
-        $viewData = []; //to be sent to the view
-        $viewData["title"] = "Create product";
+        $viewData = []; // to be sent to the view
+        $viewData['title'] = 'Create product';
 
-        return view('product.create')->with("viewData", $viewData);
+        return view('product.create')->with('viewData', $viewData);
     }
 
     public function save(Request $request)
     {
         $request->validate([
-            "name" => "required",
-            "price" => "required|numeric|gt:0"
+            'name' => 'required',
+            'price' => 'required|numeric|gt:0',
         ]);
+        Product::create($request->only(['name', 'price']));
 
-        // dd($request->all());
         return redirect()->route('product.created');
+        // return back()
     }
 
-        public function created()
+    public function created()
     {
         return view('product.created');
     }
 }
-
